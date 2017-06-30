0cce0088bfff1e73efa1da336ec8ac728d3312da
ddadai <ddadai@163.com>
2017-06-30 15:19:40 +0800

    1

---
+++ testlib/src/main/java/com/ddd/testlib/TestUtil.java
@@ -0,0 +1,12 @@
+package com.ddd.testlib;
+
+/**
+ * Created by shi on 2017/6/30.
+ */
+
+public class TestUtil {
+
+    public static final String getString(){
+        return "测试github lib";
+    }
+}
